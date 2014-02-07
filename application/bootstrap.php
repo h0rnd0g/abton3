194766da44eba92f3fc27777b93a9fa904195705
deus krid <deus.krid@gmail.com>
2014-02-07 17:23:20 +0200

    without log
    ok zaebok

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -114,6 +114,7 @@ Kohana::init(array(
  * Attach the file write to logging. Multiple writers are supported.
  */
 Kohana::$log->attach(new Log_File(APPPATH.'logs'));
+Kohana2::$log->attach(new Log_File(APPPATH.'logs'));
 
 /**
  * Attach a file reader to config. Multiple readers are supported.
